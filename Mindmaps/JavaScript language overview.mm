0654de768c42dc478d80c1fbfb69f0041c755bc3
Mardoqueu Sousa <mdq767@gmail.com>
2022-11-07 11:17:43 -0300

    Javascript: Data types

---
+++ Mindmaps/JavaScript language overview.mm
@@ -0,0 +1,137 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1667827749257" ID="ID_1282953528" MODIFIED="1667829636383" TEXT="Overview ">
+<font BOLD="true" NAME="SansSerif" SIZE="20"/>
+<node CREATED="1667828255367" ID="ID_80259055" MODIFIED="1667829642662" POSITION="right" TEXT="What is JavaScript">
+<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+<node CREATED="1667828311638" ID="ID_1003681304" MODIFIED="1667828312182" TEXT="multi-paradigm, dynamic language with types and operators, standard built-in objects, and methods. Its syntax is based on the Java and C languages ">
+<node CREATED="1667828328332" ID="ID_1430870976" MODIFIED="1667828328694" TEXT="supports ">
+<node CREATED="1667828334409" ID="ID_888020028" MODIFIED="1667828334700" TEXT="object-oriented programming">
+<node CREATED="1667828340180" ID="ID_985237024" MODIFIED="1667828341354" TEXT="object prototypes "/>
+<node CREATED="1667828344731" ID="ID_1435653618" MODIFIED="1667828345140" TEXT="classes"/>
+</node>
+</node>
+<node CREATED="1667828379347" ID="ID_1786885467" MODIFIED="1667828380061" TEXT="First-class">
+<node CREATED="1667828444972" ID="ID_591512297" MODIFIED="1667828445459" TEXT="when functions in that language are treated like any other variable.">
+<node CREATED="1667828523779" ID="ID_101957016" MODIFIED="1667828524219" TEXT="example">
+<node CREATED="1667828538351" ID="ID_549213629" MODIFIED="1667828538351" TEXT="a function can be passed as an argument to other functions"/>
+<node CREATED="1667828544464" ID="ID_1575666298" MODIFIED="1667828545002" TEXT=" can be returned by another function"/>
+<node CREATED="1667828551411" ID="ID_207010701" MODIFIED="1667828551747" TEXT="can be assigned as a value to a variable."/>
+</node>
+<node CREATED="1667828580579" ID="ID_220656329" MODIFIED="1667828581925" TEXT="Assigning a function to a variable">
+<node CREATED="1667828613086" ID="ID_1258882607" MODIFIED="1667828613086">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="JavaScript language overview _5060195909251411284.jpeg" />
+  </body>
+</html></richcontent>
+<node CREATED="1667828760319" ID="ID_385762649" MODIFIED="1667828760786" TEXT="We assigned an Anonymous Function in a Variable, then we used that variable to invoke the function by adding parentheses () at the end.  "/>
+</node>
+</node>
+<node CREATED="1667828986540" ID="ID_1740257996" MODIFIED="1667828987131" TEXT="Passing a function as an argument">
+<node CREATED="1667828997502" ID="ID_222115013" MODIFIED="1667828997502">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="JavaScript language overview _7438113188896973112.jpeg" />
+  </body>
+</html></richcontent>
+<node CREATED="1667829021538" ID="ID_1408867092" MODIFIED="1667829022208" TEXT="We are passing our sayHello() function as an argument to the greeting() function, this explains how we are treating the function as a value.  "/>
+</node>
+</node>
+<node CREATED="1667829063257" ID="ID_141914350" MODIFIED="1667829065281" TEXT="Returning a function">
+<node CREATED="1667829090741" ID="ID_779057736" MODIFIED="1667829090741">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="JavaScript language overview _7270793959084036590.jpeg" />
+  </body>
+</html></richcontent>
+<node CREATED="1667829110879" ID="ID_1943985760" MODIFIED="1667829111274" TEXT="we are returning a function from another function"/>
+<node CREATED="1667829115331" ID="ID_4608368" MODIFIED="1667829115724" TEXT="We can return a function because functions in JavaScript are treated as values"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1667828100538" ID="ID_1779426863" MODIFIED="1667829642672" POSITION="right" TEXT="JavaScript language overview">
+<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+<node CREATED="1667827940287" ID="ID_1773859707" MODIFIED="1667829629691" TEXT="Data types">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1667829736802" ID="ID_1464816324" MODIFIED="1667829737570" TEXT="Number">
+<node CREATED="1667829831726" ID="ID_613057554" MODIFIED="1667829832699" TEXT=" used for all number values (integer and floating point) except for very big integers"/>
+</node>
+<node CREATED="1667829789155" ID="ID_1502339049" MODIFIED="1667829791298" TEXT="BigInt">
+<node CREATED="1667829843587" ID="ID_1254980625" MODIFIED="1667829844243" TEXT="used for arbitrarily large integers."/>
+</node>
+<node CREATED="1667829793800" ID="ID_317545592" MODIFIED="1667829794326" TEXT="String">
+<node CREATED="1667829850477" ID="ID_608854927" MODIFIED="1667829850892" TEXT="used to store text."/>
+</node>
+<node CREATED="1667829799076" ID="ID_640062737" MODIFIED="1667829800358" TEXT="Boolean">
+<node CREATED="1667829858385" ID="ID_102023959" MODIFIED="1667829858924" TEXT="true and false &#x2014; usually used for conditional logic."/>
+</node>
+<node CREATED="1667829803514" ID="ID_1966786588" MODIFIED="1667829804590" TEXT="Symbol">
+<node CREATED="1667829866697" ID="ID_356174597" MODIFIED="1667829867289" TEXT="used for creating unique identifiers that won&apos;t collide."/>
+</node>
+<node CREATED="1667829807544" ID="ID_1052783242" MODIFIED="1667829807937" TEXT="Undefined">
+<node CREATED="1667829877427" ID="ID_647622927" MODIFIED="1667829877804" TEXT="indicating that a variable has not been assigned a value."/>
+</node>
+<node CREATED="1667829812768" ID="ID_645647273" MODIFIED="1667829813383" TEXT="Null">
+<node CREATED="1667829896156" ID="ID_235455674" MODIFIED="1667829896750" TEXT=" indicating a deliberate non-value."/>
+</node>
+<node CREATED="1667830421960" ID="ID_364293983" MODIFIED="1667830425374" TEXT="Everything else is known as an Object.">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1667830429487" ID="ID_102780264" MODIFIED="1667830429991" TEXT="Function"/>
+<node CREATED="1667830438960" ID="ID_1418044385" MODIFIED="1667830439878" TEXT="Array"/>
+<node CREATED="1667830443236" ID="ID_1452357903" MODIFIED="1667830443635" TEXT="Date"/>
+<node CREATED="1667830446967" ID="ID_610035043" MODIFIED="1667830447368" TEXT="RegExp"/>
+<node CREATED="1667830450523" ID="ID_718735480" MODIFIED="1667830450907" TEXT="Error"/>
+</node>
+</node>
+<node CREATED="1667827950849" ID="ID_1389343821" MODIFIED="1667829629711" TEXT="Variables">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1667827955006" ID="ID_680200844" MODIFIED="1667829629711" TEXT="Operators">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1667827966955" ID="ID_571746141" MODIFIED="1667829629709" TEXT="Grammar">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1667827973303" ID="ID_1230783082" MODIFIED="1667829629709" TEXT="Control structures">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1667827986301" ID="ID_161511226" MODIFIED="1667829629706" TEXT="Objects">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1667827992386" ID="ID_1374056663" MODIFIED="1667829629706" TEXT="Arrays">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1667827999903" ID="ID_1339359246" MODIFIED="1667829629706" TEXT="Functions">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1667828005146" ID="ID_1062213181" MODIFIED="1667829629705" TEXT="Classes">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1667828013170" ID="ID_309637705" MODIFIED="1667829629704" TEXT="Asynchronous programming">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1667828018126" ID="ID_1993488643" MODIFIED="1667829629702" TEXT="Modules">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1667828022889" ID="ID_630504327" MODIFIED="1667829629698" TEXT="Language and runtime">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1667828028847" ID="ID_780417924" MODIFIED="1667829629695" TEXT="Further exploration">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+</map>
